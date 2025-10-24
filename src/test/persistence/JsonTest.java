06bd91427135e459a5fcae083375f34b03b0615f
Nick_Zhang <yzhan563@student.ubc.ca>
2025-10-23 19:16:53 -0700

    write interface JsonTest

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,5 +1,19 @@
 package persistence;
 
-public interface JsonTest {
+import model.Transaction;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+    protected void checkTransaction(String id, int amountInCents,
+                                    Transaction.Category category,
+                                    Transaction.TxnType type,
+                                    String note,
+                                    Transaction tx) {
+        assertEquals(id, tx.getId());
+        assertEquals(amountInCents, tx.getAmountInCents());
+        assertEquals(category, tx.getCategory());
+        assertEquals(type, tx.getType());
+        assertEquals(note, tx.getNote());
+    }
 }
